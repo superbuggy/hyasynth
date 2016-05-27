b1f1caeebe88579392ecfa258a251300081af694
Spicy Legato <jpqreichard@gmail.com>
2016-05-27 10:39:37 -0400

    lowers volume of test sound

--- js/synth.js
+++ js/synth.js
@@ -1,9 +1,17 @@
 //'context' creates a container, necessary for WebAudio to run
 var context = new AudioContext();
+
+
 //an oscillator creates a wave; vibrational behavior that translates into sound
 oscillator = context.createOscillator();
-//this connects the oscillator directly to speakers aka the 'destination'
-//property of 'context'
-oscillator.connect(context.destination);
+
+//gain nodes adjust the intensity of a signal. in this case it acts on the volume
+gainNode = context.createGain();
+gainNode.gain.value = 0.25;
+
+//connects oscillator to gain node, and the gain node to the output
+oscillator.connect(gainNode);
+gainNode.connect(context.destination);
+
 //starts the oscillator; this fires off the sound-producing event
 oscillator.start();
