6c4d2bc7f65664b7f0079dfe072e62df3798f866
Spicy Legato <jpqreichard@gmail.com>
2016-06-03 14:10:17 -0400

    adds ajax requests to db for each button

--- public/js/dom-manip.js
+++ public/js/dom-manip.js
@@ -2,18 +2,47 @@ $(document).ready(function(){
 
   var startButton = $('#start');
   var stopButton = $('#stop');
-  var waveSelect = $('#waveform-select');
+  var waveMenu = $('#waveform-dropdown');
+  var waveform = $('#change-wave');
+  var freqModWave = $('#change-freq-wave');
+  var loadButtons = $(".load-button");
 
-  waveSelect.change(function(){
-    hyasynth.editWave(hyasynth.randomizedSynth, waveSelect.val() );
+  waveform.click(function(){
+    hyasynth.editWave(hyasynth.loadedSynth, waveMenu.val() );
+  });
+
+  freqModWave.click(function(){
+    hyasynth.editFreqMod(hyasynth.loadedSynth, waveMenu.val() );
   });
 
   startButton.click(function(){
-    hyasynth.play(hyasynth.randomizedSynth);
+      hyasynth.play(hyasynth.loadedSynth);
   });
 
   stopButton.click(function(){
     hyasynth.stop();
   });
+
+  loadButtons.click(function(){
+      var synthMongoId = $(this).attr("data-mongo-id");
+      var synthToLoad;
+      getSynthFromDb(synthMongoId, function(response){
+        synthToLoad = response;
+        hyasynth.loadSynth( synthToLoad );
+      });
+  });
+
+  function getSynthFromDb(mongoId, callback) {
+    var returnData;
+    $.getJSON("/api/synths/" + mongoId, function (data){
+        delete data._id;
+        delete data.id;
+        returnData = data;
+    }).then(function(response){
+      callback(response);
+      return returnData;
+    });
+
+  }
 
 });
